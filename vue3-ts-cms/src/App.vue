e2c814b8bad84ad42601e18eca3771b835dd7455
2019083310 <2330053403@qq.com>
2022-03-09 14:04:12 +0800

    project init

---
+++ vue3-ts-cms/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>hello</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
+
+<style lang="less"></style>
